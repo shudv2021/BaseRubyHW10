76731431b40949caa2507d899850088f3d0d6497
shudv2021 <shudv@yandex.ru>
2021-06-22 09:57:03 +0300

    less10_att2

--- accessors.rb
+++ accessors.rb
@@ -3,18 +3,17 @@ module Accessors
     base.include(InstanceMethods)
     base.extend(ClassMethods)
   end
-  #не будут ли мешать друг другу одноименные модули из разных модулей , тафталогия конечно но
-  # иначе не могу сформулировать
   module ClassMethods
     def attr_accessor_with_history(*atributes)
       atributes.each do |atribute|
         atribute_name = "@#{atribute}".to_sym
-        define_method(atribute) { instance_varible_get(atribute_name) }
+        define_method(atribute) { instance_variable_get(atribute_name) }
         define_method("#{atribute}=".to_sym) do |value|
+          instance_variable_set(atribute_name, value)
           @arr_hisrory ||= []
-          instace_varible_set(atribute_name, value)
+          @arr_hisrory<<value
         end
-        define_method("#{atribute}_history") { instance_varible_get(@arr_history) }
+        define_method("#{atribute}_history") {@arr_hisrory}
       end
     end
 
@@ -22,9 +21,22 @@ module Accessors
   end
 
   module InstanceMethods
-    def strong_attr_accessor(atribut_name, atribure_class)
-      # Не понял что этот метод должен делать в контексте нашей проргаммы. Все проверки вроде бы в
-      # модуле Valide нужно увязать его с этим модулем или делать проверку отдельно
+    def strong_attr_accessor(atr_name, atr_class)
+      if atr_name.class == atr_class
+        atribute_name = "@#{atr_name}".to_sym
+        define_method(atr_name) {instance_variable_get(atribute_name)}
+        define_method("#{atr_name}=".to_sym) { |value| instance_variable_set(atr_name, value)}
+      end
     end
+  end
+  end
 
-end
+  class Test
+    include Accessors
+    attr_accessor_with_history(:x, :y, :z)
+  end
+
+t=Test.new
+t.x=10
+t.x=12
+t.x=30
